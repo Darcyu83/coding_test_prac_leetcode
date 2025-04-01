ce9cddc9207e34778170a7c2068922a029cebbb2
DARCY DAESEOK YU <darcy.u83@gmail.com>
2025-04-01 16:00:46 +0900

    coinChang - DP

--- src/myLeetcode15Patterns/15.DynamicProgrammingPatters/coinChange.ts
+++ src/myLeetcode15Patterns/15.DynamicProgrammingPatters/coinChange.ts
@@ -1,3 +1,18 @@
+// faster
+export function coinChange(coins: number[], amount: number): number {
+  const dp: number[] = new Array(amount + 1).fill(Infinity);
+
+  dp[0] = 0;
+
+  for (const coin of coins) {
+    for (let money = coin; money <= amount; money++) {
+      dp[money] = Math.min(dp[money], dp[money - coin] + 1);
+    }
+  }
+
+  return dp[amount] === Infinity ? -1 : dp[amount];
+}
+
 // DP (bottom-up)
 export function coinChangeDP(coins: number[], amount: number): number {
   const dp: number[] = new Array(amount + 1).fill(amount + 1);
@@ -14,32 +29,30 @@ export function coinChangeDP(coins: number[], amount: number): number {
   return dp[amount] === amount + 1 ? -1 : dp[amount];
 }
 
-// BFS 
+// BFS
 export function coinChangeBFS(coins: number[], amount: number): number {
   if (amount === 0) return 0;
-
   const queue: number[] = [amount];
-  let visited: Set<number> = new Set();
+  const visited: Set<number> = new Set();
   let numberOfCoins = 0;
 
   while (queue.length > 0) {
     let size = queue.length;
     numberOfCoins++;
 
-    for (let i = 0; i < size; i++) {
-      let remaining = queue.shift()!;
-      for (let coin of coins) {
-        let newAmount = remaining - coin;
+    while (size > 0) {
+      size--;
+      const remaining = queue.shift()!;
+      for (const coin of coins) {
+        const newRemaining = remaining - coin;
+        if (newRemaining === 0) return numberOfCoins;
 
-        if (newAmount === 0) return numberOfCoins;
-
-        if (newAmount > 0 && !visited.has(newAmount)) {
-          visited.add(newAmount);
-          queue.push(newAmount);
+        if (newRemaining > 0 && !visited.has(newRemaining)) {
+          visited.add(newRemaining);
+          queue.push(newRemaining);
         }
       }
     }
   }
-
   return -1;
 }
